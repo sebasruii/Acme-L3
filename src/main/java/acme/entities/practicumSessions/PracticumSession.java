d0d33804087536042f154302dd7436d2b3fd3bbd
lucperrom1 <lucperrom1@alum.us.es>
2023-03-01 20:53:10 +0100

    Student4-Create PracticumSession-Se añadió relación bidireccional con practicum-Issue#20

---
+++ src/main/java/acme/entities/practicumSessions/PracticumSession.java
@@ -0,0 +1,70 @@
+
+package acme.entities.practicumSessions;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.entities.practicums.Practicum;
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "practicumSession")
+public class PracticumSession extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			sumary;
+
+	@NotNull
+	protected LocalDate			startDate;
+
+	@NotNull
+	protected LocalDate			finishDate;
+
+	@URL
+	protected String			link;
+
+
+	@Transient
+	public boolean IsValid() {
+		boolean result = false;
+		final Period period1 = Period.between(this.finishDate, this.startDate);
+		final Period period2 = Period.between(this.startDate, LocalDate.now());
+		if (period1.getDays() >= 7 && period2.getDays() >= 7)
+			result = true;
+		return result;
+	}
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+
+	@ManyToOne()
+	@JoinColumn(name = "practicum_id")
+	private Practicum practicum;
+
+}
--- src/main/java/acme/entities/practicums/Practicum.java
+++ src/main/java/acme/entities/practicums/Practicum.java
@@ -2,14 +2,18 @@
 package acme.entities.practicums;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Length;
 
+import acme.entities.practicumSessions.PracticumSession;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,30 +21,37 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-
+@Table(name = "practicum")
 public class Practicum extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 
-	protected static final long	serialVersionUID	= 1L;
+	protected static final long		serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 	@NotBlank
 	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
-	protected String			code;
+	protected String				code;
 
 	@NotBlank
 	@Size(max = 75)
-	protected String			title;
+	protected String				title;
 
 	@NotBlank
 	@Size(max = 100)
-	protected String			summary;
+	protected String				summary;
 
 	@NotBlank
 	@Length(max = 100)
-	protected ArrayList<String>	goals;
+	protected ArrayList<String>		goals;
 
-	protected Integer			estimedTotalTime;
+	protected Integer				estimedTotalTime;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@OneToMany(/* cascade = CascadeType.ALL, mappedBy = "practicumSession" */)
+	private List<PracticumSession>	practicumSessions;
 
 }
