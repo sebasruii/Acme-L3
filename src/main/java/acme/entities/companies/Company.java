d3152552297dec7263d5413a3b7beb6805176a54
lucperrom1 <lucperrom1@alum.us.es>
2023-03-01 17:38:08 +0100

    Create Company-DONE-Issue#16

--- src/main/java/acme/entities/companies/Company.java
+++ src/main/java/acme/entities/companies/Company.java
@@ -7,26 +7,37 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.URL;
 
+import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Company {
+public class Company extends AbstractEntity {
+	// Serialisation identifier -----------------------------------------------
 
-	@NotBlank
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
 	@Size(max = 75)
-	protected String	name;
-
 	@NotBlank
-	@Size(max = 26)
-	protected String	vat;
+	protected String			name;
 
+	@Size(max = 25)
 	@NotBlank
+	protected String			VAT;
+
 	@Size(max = 100)
-	protected String	summary;
+	@NotBlank
+	protected String			summary;
 
 	@URL
-	protected String	link;
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
 }
