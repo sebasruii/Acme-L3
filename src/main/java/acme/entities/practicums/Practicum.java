a4be392424c63287ca0754bedc7f33f9dc9a20b0
lucperrom1 <lucperrom1@alum.us.es>
2023-03-01 20:00:42 +0100

    Student4-Create Practicum-DONE-Issue#17

---
+++ src/main/java/acme/entities/practicums/Practicum.java
@@ -0,0 +1,46 @@
+
+package acme.entities.practicums;
+
+import java.util.ArrayList;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class Practicum extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Size(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Size(max = 100)
+	protected String			summary;
+
+	@NotBlank
+	@Length(max = 100)
+	protected ArrayList<String>	goals;
+
+	protected Integer			estimedTotalTime;
+
+}
