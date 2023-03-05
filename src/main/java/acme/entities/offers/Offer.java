5329fcaa54bf48c7e3ab43a5ed8d9ddd6f44b216
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2023-03-05 11:07:02 +0100

    Corrección offer

--- src/main/java/acme/entities/offers/Offer.java
+++ src/main/java/acme/entities/offers/Offer.java
@@ -1,28 +1,20 @@
 
 package acme.entities.offers;
 
-import java.time.Duration;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.Transient;
-import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.components.datatypes.Money;
 import acme.framework.data.AbstractEntity;
-import acme.framework.helpers.MomentHelper;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -44,26 +36,26 @@ public class Offer extends AbstractEntity {
 	protected Date				instantiation;
 
 	@NotBlank
-	@Size(max = 75)
+	@Length(max = 75)
 	protected String			heading;
 
 	@NotBlank
-	@Size(max = 100)
+	@Length(max = 100)
 	protected String			summary;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
 	protected Date				availabilityInit;
-    
+
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
 	protected Date				availabilityFinish;
-	
+
 	@NotNull
-	protected Money	price;
+	protected Money				price;
 
 	@URL
-	protected String	link;
+	protected String			link;
 
 	// Derived attributes -----------------------------------------------------
 
