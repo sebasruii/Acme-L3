3ce576176ac1512d90d94a785ec48ae4ffe0427b
JuanAntonioJimenezdelVillar <juajimdel2@alum.us.es>
2023-03-06 19:06:09 +0100

    Student2 TI5: Create an enrolment #40

---
+++ src/main/java/acme/entities/enrolment/Enrolment.java
@@ -0,0 +1,37 @@
+
+package acme.entities.enrolment;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Enrolment extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			motivation;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			goals;
+
+}
