6c307535e6bfe3b6b0d960986bebb4b9f31132a5
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2023-03-08 17:45:09 +0100

    Pattern changed

--- src/main/java/acme/entities/tutorials/Tutorial.java
+++ src/main/java/acme/entities/tutorials/Tutorial.java
@@ -34,7 +34,7 @@ public class Tutorial extends AbstractEntity {
 
 	@Column(unique=true)
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	@Pattern(regexp = "[A-Z]{1,3}\\d\\d{3}")
 	protected String			code;
 
 	@NotBlank
