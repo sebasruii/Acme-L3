f948930dd92b831613b9a5e7dda79e1c20aa1533
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2023-03-09 09:57:39 +0100

    update tutorial

--- src/main/java/acme/entities/tutorials/Tutorial.java
+++ src/main/java/acme/entities/tutorials/Tutorial.java
@@ -3,6 +3,7 @@ package acme.entities.tutorials;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
@@ -28,7 +29,7 @@ public class Tutorial extends AbstractEntity {
 	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
-
+	@Column(unique = true)
 	@NotBlank
 	@Pattern(regexp = "[A-Z]{1,3}\\d\\d{3}")
 	protected String			code;
