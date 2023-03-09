720a33215600d034ba2932cd6b41f1cc16f5a340
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2023-03-09 09:55:52 +0100

    update tutorial

--- src/main/java/acme/entities/tutorials/Tutorial.java
+++ src/main/java/acme/entities/tutorials/Tutorial.java
@@ -1,19 +1,17 @@
 
 package acme.entities.tutorials;
 
-import java.sql.Time;
+import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
 
 import acme.entities.courses.Course;
 import acme.framework.data.AbstractEntity;
@@ -31,29 +29,25 @@ public class Tutorial extends AbstractEntity {
 
 	// Attributes -------------------------------------------------------------
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	protected int				id;
-
-	@NotBlank()
+	@NotBlank
 	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
 	protected String			code;
 
-	@NotBlank()
-	@Size(max = 75)
+	@NotBlank
+	@Length(max = 75)
 	protected String			title;
 
-	@NotBlank()
-	@Size(max = 100)
+	@NotBlank
+	@Length(max = 100)
 	protected String			summary;
 
-	@NotBlank()
-	@Size(max = 100)
+	@NotBlank
+	@Length(max = 100)
 	protected String			goals;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
-	protected Time				estimated_time;
+	protected Date				estimated_time;
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
