4cdc1900da921481be9345d6144b9dc111511228
María Márquez <marmarmar73@alum.us.es>
2023-03-09 08:37:49 +0100

    Cambio de Size a Length en Bulletin

--- src/main/java/acme/entities/bulletins/Bulletin.java
+++ src/main/java/acme/entities/bulletins/Bulletin.java
@@ -9,8 +9,8 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractEntity;
@@ -31,11 +31,11 @@ public class Bulletin extends AbstractEntity {
 	protected Date				moment;
 
 	@NotBlank
-	@Size(max = 75)
+	@Length(max = 75)
 	protected String			title;
 
 	@NotBlank
-	@Size(max = 100)
+	@Length(max = 100)
 	protected String			message;
 
 	protected Boolean			critical;
