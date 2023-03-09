e3e1fce5508c1d6af7c5d06e968683e169d89ebf
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2023-03-09 09:15:53 +0100

    Relacion con courses

--- src/main/java/acme/entities/tutorials/Tutorial.java
+++ src/main/java/acme/entities/tutorials/Tutorial.java
@@ -3,11 +3,11 @@ package acme.entities.tutorials;
 
 import java.sql.Time;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
@@ -15,6 +15,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import acme.entities.courses.Course;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,7 +23,6 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-
 public class Tutorial extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
@@ -31,29 +31,32 @@ public class Tutorial extends AbstractEntity {
 
 	// Attributes -------------------------------------------------------------
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	protected int				id;
 
-	@Column(unique=true)
-	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}\\d\\d{3}")
+	@NotBlank()
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
 	protected String			code;
 
-	@NotBlank
+	@NotBlank()
 	@Size(max = 75)
 	protected String			title;
 
-	@NotBlank
+	@NotBlank()
 	@Size(max = 100)
 	protected String			summary;
 
-	@NotBlank
+	@NotBlank()
 	@Size(max = 100)
 	protected String			goals;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
-	protected Time				estimatedTime;
+	protected Time				estimated_time;
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
-
+	@ManyToOne
+	protected Course			course;
 }
