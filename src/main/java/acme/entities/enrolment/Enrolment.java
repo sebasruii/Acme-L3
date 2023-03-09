3a689b3919397909df4638d77de704a79c01c56e
JuanAntonioJimenezdelVillar <juajimdel2@alum.us.es>
2023-03-09 09:12:34 +0100

    fix: hacer code unico

--- src/main/java/acme/entities/enrolment/Enrolment.java
+++ src/main/java/acme/entities/enrolment/Enrolment.java
@@ -1,6 +1,7 @@
 
 package acme.entities.enrolment;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -22,6 +23,7 @@ public class Enrolment extends AbstractEntity {
 
 	// Attributes -------------------------------------------------------------
 
+	@Column(unique = true)
 	@NotBlank
 	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
 	protected String			code;
