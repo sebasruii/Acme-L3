3ed661985b158423e36603b314b58053be907c1b
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2023-03-09 09:56:47 +0100

    update turrial

--- src/main/java/acme/entities/tutorials/Tutorial.java
+++ src/main/java/acme/entities/tutorials/Tutorial.java
@@ -30,7 +30,7 @@ public class Tutorial extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	@Pattern(regexp = "[A-Z]{1,3}\\d\\d{3}")
 	protected String			code;
 
 	@NotBlank
