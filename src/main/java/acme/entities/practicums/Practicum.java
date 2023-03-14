121d92ee35a75710af4839b1ad464a3ac175e93a
Lucía Pérez <100713036+Lucperrom@users.noreply.github.com>
2023-03-14 17:41:37 +0100

    Corrección

--- src/main/java/acme/entities/practicums/Practicum.java
+++ src/main/java/acme/entities/practicums/Practicum.java
@@ -27,7 +27,7 @@ public class Practicum extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 	@Column(unique=true)
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}\\d\\d{3}")
+	@Pattern(regexp = "^[A-Z]{1,3}\\d\\d{3}$")
 	protected String			code;
 
 	@NotBlank
@@ -40,6 +40,7 @@ public class Practicum extends AbstractEntity {
 
 	@NotBlank
 	@Length(max = 100)
-	protected ArrayList<String>	goals;
+	protected String	goals;
+
 
 }
