95426286adde6e9b37dc10ded45c321d2ba61965
María Márquez <marmarmar73@alum.us.es>
2023-03-14 17:56:18 +0100

    feat: Administrator Dashboard

---
+++ src/main/java/acme/forms/AdministratorDashboard.java
@@ -0,0 +1,24 @@
+
+package acme.forms;
+
+import java.util.Map;
+
+import acme.framework.components.datatypes.Money;
+import acme.framework.data.AbstractForm;
+import acme.forms.Statistic
+
+public class AdministratorDashboard extends AbstractForm {
+
+	protected static final long		serialVersionUID	= 1L;
+
+	private Map<String, Integer>	totalUsers;
+
+	private Double					peepsRatio;
+
+	private Double					bulletinsRatio;
+
+	private Map<Money, Statistic>	offerBudgetByCurrency;
+
+	private Statistic				notesInLast10WeeksData;
+
+}
