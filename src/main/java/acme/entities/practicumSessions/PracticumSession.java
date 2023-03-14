bd3b124dfa6b7aea6a1e85040217a6f38e150971
Lucía Pérez <100713036+Lucperrom@users.noreply.github.com>
2023-03-14 18:00:07 +0100

    corrección

--- src/main/java/acme/entities/practicumSessions/PracticumSession.java
+++ src/main/java/acme/entities/practicumSessions/PracticumSession.java
@@ -2,10 +2,7 @@
 package acme.entities.practicumSessions;
 
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.Valid;
