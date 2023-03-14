aa483061de70e700a32951ee39f0c0aa7463f87c
Francisco Benítez <frabenrui1@alum.us.es>
2023-03-14 16:58:09 +0100

    statistics created

---
+++ src/main/java/acme/forms/LecturerDashboard.java
@@ -0,0 +1,12 @@
+
+package acme.forms;
+
+import acme.framework.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LecturerDashboard extends AbstractForm {
+
+}
