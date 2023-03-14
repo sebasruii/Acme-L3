26481ff8b2d2d7355f5edcedd48298c7d718a282
JuanAntonioJimenezdelVillar <juajimdel2@alum.us.es>
2023-03-14 10:50:16 +0100

    fix: Corrección de errores en la relación con student.

--- src/main/java/acme/entities/enrolment/Enrolment.java
+++ src/main/java/acme/entities/enrolment/Enrolment.java
@@ -4,7 +4,9 @@ package acme.entities.enrolment;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
@@ -40,6 +42,8 @@ public class Enrolment extends AbstractEntity {
 
 	// Relationships ---------------------------------------------------------
 
-	@ManyToOne
+	@Valid
+	@NotNull
+	@ManyToOne(optional = false)
 	protected Student			student;
 }
