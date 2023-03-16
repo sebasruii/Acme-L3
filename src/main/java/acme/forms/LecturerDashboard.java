24e7b9fdc2b963675fe5d2724fb936357223fe75
Francisco Benítez <frabenrui1@alum.us.es>
2023-03-16 16:52:14 +0100

    change protected for private

--- src/main/java/acme/forms/LecturerDashboard.java
+++ src/main/java/acme/forms/LecturerDashboard.java
@@ -12,11 +12,11 @@ import lombok.Setter;
 public class LecturerDashboard extends AbstractForm {
 	// Serialisation identifier -----------------------------------------------
 
-	protected static final long			serialVersionUID	= 1L;
+	protected static final long		serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 
-	protected Map<String, Statistics>	statisticsByLectureType;
-	protected Statistics				statisticsByCourse;
+	private Map<String, Statistics>	statisticsByLectureType;
+	private Statistics				statisticsByCourse;
 
 }
