d557d51414080f2b087e3d29e09100ec6ce4e357
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2023-03-16 16:37:48 +0100

    Created relationships assistant-tutorial

--- src/main/java/acme/entities/tutorials/Tutorial.java
+++ src/main/java/acme/entities/tutorials/Tutorial.java
@@ -1,13 +1,10 @@
 
 package acme.entities.tutorials;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -31,7 +28,7 @@ public class Tutorial extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 	@Column(unique = true)
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}\\d\\d{3}")
+	@Pattern(regexp = "^[A-Z]{1,3}\\d{4}$")
 	protected String			code;
 
 	@NotBlank
@@ -46,12 +43,11 @@ public class Tutorial extends AbstractEntity {
 	@Length(max = 100)
 	protected String			goals;
 
-	@Temporal(TemporalType.TIMESTAMP)
-	@NotNull
-	protected Date				estimated_time;
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
-	@ManyToOne
+	@ManyToOne(optional = false)
+	@Valid
+	@NotNull
 	protected Course			course;
 }
